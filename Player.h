887a4167fce83b22cb4e3fe807e77e8fda0f6c6a
SaintYu0421 <126647113+SaintYu0421@users.noreply.github.com>
2023-04-20 18:05:17 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "Card.h"
 
+class Player {
+public:
+Player(); // 初始化
+void hit(Card &card); // 玩家補牌
+void printCards(); // 列印所有牌
+int getSum(); // 取得牌點數總和
+bool isBust(); // 是否爆牌
+void reset(); // 重置牌局
+private:
+vector<Card> cards; // 玩家的牌
+};
+
+#endif
