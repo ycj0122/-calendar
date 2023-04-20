4801d986931c20ae58cb46910d25e42aa93bab54
ycj0122 <126649186+ycj0122@users.noreply.github.com>
2023-04-20 13:33:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,23 +6,23 @@ using namespace std;
 int main() {
 	Card card("black", 12);
 	string color;
-	int math,x=-1,total=0;
+	int math, x = -1, total = 0;
 	while (x != 0) {
 		cin >> color >> math;
 		card.setColor(color);
 		card.setMath(math);
+		total += card.getMath();
 		card.setSum(total);
-		total += card.getSum();
 		card.print();
 		if (total <= 21) {
-			cout << "¬O§_¥[µP?\n¬O½Ð¿é¤J1\n§_½Ð¿é¤J0" << endl;
+			cout << "æ˜¯å¦åŠ ç‰Œ?\næ˜¯è«‹è¼¸å…¥1\nå¦è«‹è¼¸å…¥0" << endl;
 			cin >> x;
 		}
 		else {
 			cout << "Game Over" << endl;
 			x = 0;
 		}
-		
+
 	}
 	return 0;
 }
