c142ba4c6af170742b7ee6111dd0bc268b500a16
OMD0118 <115619547+OMD0118@users.noreply.github.com>
2023-04-22 02:30:02 +0800

    Update Dealar.h

--- Dealar.h
+++ Dealar.h
@@ -1,1 +1,20 @@
+#ifndef DEALER_H
+#define DEALER_H
+#include "Sum.h"
+#include <vector>
+using namespace std;
+class Dealer {
+public:
+	Dealer();
+	void hit();
+	void printCards();
+	int getSum();
+	int getNumCards();
+	bool isBust();
+	bool shouldHit();
+	void reset();
+private:
+	vector<Sum> cards;
+};
 
+#endif
