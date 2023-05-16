15e2046ae3c6f6b7ac4fdb50fc38c88fd6f84271
ycj0122 <126649186+ycj0122@users.noreply.github.com>
2023-05-16 23:12:02 +0800

    Update Sum.cpp

--- Sum.cpp
+++ Sum.cpp
@@ -1,13 +1,10 @@
 #include"Sum.h"
-Sum::Sum(int color, int math) {
+Sum::Sum(string color, int math) {
 	this->color = color;
 	this->math = math;
 }
-void Sum::setColor(int c) {
-	if (c == 0) color = "黑桃";
-	else if (c == 1) color = "紅心";
-	else if (c == 2) color = "方塊";
-	else color = "梅花";
+void Sum::setColor(string color) {
+	this->color = color;
 }
 string Sum::getColor() {
 	return color;
@@ -16,8 +13,17 @@ void Sum::setMath(int math) {
 	this->math = math;
 }
 int Sum::getMath() {
+	int a;
 	if (math == 11 || math == 12 || math == 13)
 		math = 10;
+	else if (math == 1) {
+		cout << "A為1 or 11?" << endl;
+		cin >> a;
+		if (a == 1)
+			math = 1;
+		else
+			math = 11;
+	}
 	return math;
 }
 void Sum::setSum(int sum) {
