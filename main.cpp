7e6cd546579f4f55d0d4bff243a6b5e66c21446a
ycj0122 <126649186+ycj0122@users.noreply.github.com>
2023-05-16 23:12:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,38 +2,85 @@
 #include<iostream>
 #include<iomanip>
 #include<cstdlib>
+#include<sstream>
 #include<ctime>
+#include<algorithm>
 #include"Sum.h"
 using namespace std;
-int main() {
-	Sum card(0, 12);
-	int math, x = -1, total = 0;
-	int cardArray[4][13] = {};
-	srand(time(0));
+Sum card("花色",10);
+string*  Card(string cardArray[]) {		//建立牌組並洗牌
+	string* r = new string[52];
+	string colorArray[4]={"黑桃","紅心","方塊","梅花"};
+	string mathArray[13] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+	int n = 0;
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 13; j++) {
-			cardArray[i][j] = j+1;
+			string color = colorArray[i];
+			string math = mathArray[j];
+			string cards ;
+			cards.append(color).append(" ").append(math);
+			r[n] = cards;
+			n++;
 		}
 	}
-	while (x != 0) {
-		int c =rand() % 4, math=rand() % 13;
-		if (cardArray[c][math] != 0) {
-			card.setColor(c);
-			card.setMath(math+1);
-			cout << card.getColor() << " " << card.getMath() << endl;
-			total += card.getMath();
+	random_shuffle(r, r + 52);
+	for (int i = 0; i < 52; i++) {
+		cardArray[i] = r[i];
+	}
+	delete[] r;
+	return cardArray;
+}
+
+int draw(string cardArray[], int total) {	//抽牌並帶入加總物件
+	int x = 0;
+	srand(time(0));
+	while (x!=-1) {
+		int number= rand() % 52;
+		stringstream ss(cardArray[number]);
+		string word[2];
+		int i = 0, math;
+		while (!ss.eof()) {
+			ss >> word[i];
+			i++;
+		}
+		if (word[1] == "J" || word[1] == "Q" || word[1] == "K")
+			math = 10;
+		else if (word[1] == "A") 
+			math = 1;
+		else
+			math = stoi(word[1]);
+		if (math != 0) {
+			card.setColor(word[0]);
+			card.setMath(math);
+			total += math;
 			card.setSum(total);
-			cout << "目前點數:" << card.getSum() << endl;
-			cardArray[c][math] = 0;
-			if (total <= 21) {
-				cout<< "是否加牌?\n是請輸入1\n否請輸入0" << endl;
-				cin >> x;
-			}
-			else {
-				x = 0;
-				cout << "Game Over" << endl;
-			}
+			cout << card.getColor() << " " << card.getMath()<< endl
+			<< "目前點數:" << card.getSum()<< endl;
+			x = -1;
+		}
+		else if (card.getSum() > 21) {
+			x = -1;
 		}
 	}
+	return card.getSum();
+}
+
+int main() {
+	cout << "Game Start" << endl;
+	string cardArray[52];
+	Card(cardArray);
+	int total = 0,x = 1;
+	while (x != 0) {
+		total=draw(cardArray,total); 
+		if (total > 21) {
+			cout  <<"Game Over\n";
+			x = 0;
+		}
+		else {
+			cout << "是否加牌?\n是請輸入1\n否請輸入0" << endl;		
+			cin >> x;
+		}
+	}
+	cout << "總點數:" <<total<< endl;
 	return 0;
 }
