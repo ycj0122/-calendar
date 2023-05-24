8b846485c60d49fadc3559a624b00372bd9ce789
SaintYu0421 <126647113+SaintYu0421@users.noreply.github.com>
2023-05-24 13:49:31 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,43 +1,54 @@
 #include "Player.h"
 
-Player::Player() {}
-
-void Player::hit(Card &card) {
-// 玩家補牌
-cards.push_back(card);
+Player::Player() {
+    name = "Player";
 }
 
-void Player::printCards() {
-// 列印所有牌
-cout << "Player: " << endl;
-for (int i = 0; i < cards.size(); i++) {
-cards[i].print();
-}
+Player::Player(const string& name) {
+    this->name = name;
 }
 
-int Player::getSum() {
-// 取得牌點數總和
-int sum = 0;
-int aceCount = 0;
-for (int i = 0; i < cards.size(); i++) {
-sum += cards[i].getMath();
-if (cards[i].getMath() == 1) {
-aceCount++;
-}
-}
-while (aceCount > 0 && sum + 10 <= 21) {
-sum += 10;
-aceCount--;
-}
-return sum;
+void Player::setName(const string& name) {
+    this->name = name;
 }
 
-bool Player::isBust() {
-// 是否爆牌
-return getSum() > 21;
+string Player::getName() const {
+    return name;
 }
 
-void Player::reset() {
-// 重置牌局
-cards.clear();
+void Player::addCard(const string& card) {
+    Sum newCard(card);
+    cards.push_back(newCard);
+}
+
+void Player::clearCards() {
+    cards.clear();
+}
+
+int Player::getTotalSum() const {
+    int totalSum = 0;
+    int numOfAces = 0;
+
+    for (const auto& card : cards) {
+        int math = card.getMath();
+        totalSum += math;
+
+        if (math == 1)
+            numOfAces++;
+    }
+
+    while (numOfAces > 0 && totalSum + 10 <= 21) {
+        totalSum += 10;
+        numOfAces--;
+    }
+
+    return totalSum;
+}
+
+void Player::printCards() const {
+    cout << "=== " << name << "的牌 ===" << endl;
+    for (const auto& card : cards) {
+        card.print();
+    }
+    cout << "總點數: " << getTotalSum() << endl;
 }
