1cc578e0fcf0fe6943b6794e9053ab9fbc4cc246
OMD0118 <115619547+OMD0118@users.noreply.github.com>
2023-05-31 15:22:47 +0800

    Update Dealer.cpp

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,1 +1,30 @@
 #include "Dealer.h"
+
+Dealer::Dealer() {}
+
+void Dealer::printCards() {
+	cout << "Dealer's Cards: " << endl;
+	for (const Sum& card : cards) {
+		card.print();
+	}
+}
+
+int Dealer::getSum() {
+	int total = 0;
+	for (const Sum& card : cards) {
+		total += card.getMath();
+	}
+	return total;
+}
+
+int Dealer::getNumCards() {
+	return cards.size();
+}
+
+bool Dealer::isBust() {
+	return getSum() > 21;
+}
+
+void Dealer::reset() {
+	cards.clear();
+}
