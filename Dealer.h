3225b574bf47370a62b3d00f373f99591023a80e
OMD0118 <115619547+OMD0118@users.noreply.github.com>
2023-06-03 22:43:35 +0800

    Update Dealer.h

--- Dealer.h
+++ Dealer.h
@@ -1,17 +1,22 @@
 #ifndef DEALER_H
 #define DEALER_H
+#include <string>
+#include <iostream>
 #include "Sum.h"
-#include <vector>
 using namespace std;
 class Dealer {
 public:
-	Dealer();
-	void printCards();
-	int getSum();
-	int getNumCards();
-	bool isBust();
-	void reset();
+    Dealer();
+    Dealer(string);
+    void setCard(string);//抽到的牌
+    string getCard()const;
+    void setNowpoint(int);//目前點數
+    int getNowpont();
+    void setTotalpoint(int);//總點數
+    int getTotalpoint();
 private:
-	vector<Sum> cards;
+    string card;
+    string cardArray[5];
+    int point;
 };
 #endif
