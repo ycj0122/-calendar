8bca7fb7a6f6f1c41e38662272e491f45d00832b
SaintYu0421 <126647113+SaintYu0421@users.noreply.github.com>
2023-06-04 18:15:23 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,37 +1,25 @@
-#include "Player.h"
-
+#include"Player.h"
 Player::Player() {
+
 }
-
-void Player::addCard(const string& card) {
-    Sum newCard(card);
-    cards.push_back(newCard);
+Player::Player(string card) {
+    setCard(card);
 }
-
-bool Player::getTotalSum() const {
-    int totalSum = 0;
-    int numOfAces = 0;
-
-    for (const auto& card : cards) {
-        int math = card.getMath();
-        totalSum += math;
-
-        if (math == 1)
-            numOfAces++;
-    }
-
-    while (numOfAces > 0 && totalSum + 10 <= 21) {
-        totalSum += 10;
-        numOfAces--;
-    }
-
-    return totalSum;
+void Player::setCard(string card) {		//抽到的牌
+    this->card = card;
 }
-
-void Player::printCards() const {
-    cout << "=== " << name << "的牌 ===" << endl;
-    for (const auto& card : cards) {
-        card.print();
-    }
-    cout << "總點數: " << getTotalSum() << endl;
+string Player::getCard()const {
+    return card;
+}
+void Player::setNowpoint(int point) {    //目前點數
+    this->point = point;
+}
+int Player::getNowpont() {
+    return point;
+}
+void Player::setTotalpoint(int totalpoint) {        //總點數
+    this->totalpoint = totalpoint;
+}
+int Player::getTotalpoint() {
+    return totalpoint;
 }
